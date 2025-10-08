55c0394fb253d90966ba0d7657e3a5bea4ca2d49
Zane Rockenbaugh <zane@liquid-labs.com>
2025-10-08 13:20:02 -0500

    fix compilation of test data

--- make/20-js-src-finder.mk
+++ make/20-js-src-finder.mk
@@ -5,6 +5,8 @@ SDLC_JS_SELECTOR=\( -name "*.js" -o -name "*.cjs" -o -name "*.mjs" \)
 SDLC_TEST_SELECTOR=\( -name "*.test.*js" -o -path "*/test/*" \)
 
 # all source, non-test files (cli and lib)
-SDLC_ALL_JS_FILES_SRC:=$(shell find $(SRC) $(SDLC_JS_SELECTOR) -not $(SDLC_DATA_SELECTOR) -type f)
+# In our case, we actually want to include 'src/test/data/*' files, so we don't use the SDLC_DATA_SELECTOR
+# SDLC_ALL_JS_FILES_SRC:=$(shell find $(SRC) $(SDLC_JS_SELECTOR) -not $(SDLC_DATA_SELECTOR) -type f)
+SDLC_ALL_JS_FILES_SRC:=$(shell find $(SRC) $(SDLC_JS_SELECTOR) -type f)
 SDLC_ALL_NON_TEST_JS_FILES_SRC:=$(shell find $(SRC) $(SDLC_JS_SELECTOR) -not $(SDLC_DATA_SELECTOR) -not $(SDLC_TEST_SELECTOR) -type f)
 SDLC_JS_TEST_FILES_BUILT:=$(patsubst %.cjs, %.js, $(patsubst %.mjs, %.js, $(patsubst $(SRC)/%, test-staging/%, $(SDLC_ALL_JS_FILES_SRC))))
