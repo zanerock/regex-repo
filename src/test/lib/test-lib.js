04612d724b33c6198995282322c0a02515e8c7d7
Zane Rockenbaugh <zane@liquid-labs.com>
2025-10-08 11:52:03 -0500

    documented test function

--- src/test/lib/test-lib.js
+++ src/test/lib/test-lib.js
@@ -35,6 +35,43 @@ export const groupTestPartial = (reString, validData, invalidData, desc, prefix 
   groupTestHelper(re, invalidData.map((d) => prefix + d + suffix), false, desc + ' partial match')
 }
 
+/**
+ * Tests that a regular expression correctly extracts capture groups from input strings.
+ *
+ * This function supports two modes:
+ * 1. Sequential mode: Tests capture groups 1, 2, 3, ... in order (when groupNumbers is omitted)
+ * 2. Numbered mode: Tests specific capture group numbers (when groupNumbers is provided)
+ *
+ * The numbered mode is useful for regexes with internal non-capturing groups, backreferences,
+ * or alternations that create sparse or non-sequential capture group numbering.
+ *
+ * @param {RegExp} re - The regular expression to test
+ * @param {string[]} inputs - Array of input strings to test against the regex
+ * @param {Array<Array<string|undefined>>} expectedMatches - Array of expected capture group values for each input.
+ *        Each inner array contains the expected values for the capture groups being tested.
+ * @param {number[]} [groupNumbers] - Optional array of capture group numbers to test.
+ *        If omitted, tests groups 1, 2, 3, ... sequentially.
+ * @param {string} desc - Description of the test (shown in test output)
+ *
+ * @example
+ * // Sequential mode (tests groups 1, 2, 3)
+ * testCaptureGroups(
+ *   /(\d{4})-(\d{2})-(\d{2})/,
+ *   ['2024-01-15'],
+ *   [['2024', '01', '15']],
+ *   'date capture groups'
+ * )
+ *
+ * @example
+ * // Numbered mode (tests specific group numbers: 1, 3, 5)
+ * testCaptureGroups(
+ *   /^(https?):\/\/((?:\w+\.)*\w+)(:\d+)?(\/.*)?$/,
+ *   ['https://example.com:8080/path'],
+ *   [['https', 'example.com', ':8080', '/path']],
+ *   [1, 2, 3, 4], // explicitly specify which groups to test
+ *   'URL capture groups'
+ * )
+ */
 export const testCaptureGroups = (re, inputs, expectedMatches, groupNumbers, desc) => {
   // Handle optional groupNumbers parameter
   // If groupNumbers is a string, it's actually the desc parameter
@@ -61,4 +98,5 @@ export const testCaptureGroups = (re, inputs, expectedMatches, groupNumbers, des
   })
 }
 
+// Backward compatibility alias
 export const testCaptureGroupsByNumber = testCaptureGroups
