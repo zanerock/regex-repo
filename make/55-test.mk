084be6cc105ddeef7c434427100bd7ad690ab2ae
Zane Rockenbaugh <zane@liquid-labs.com>
2025-10-08 14:14:39 -0500

    fix dependency (test data built, not copied)

--- make/55-test.mk
+++ make/55-test.mk
@@ -29,7 +29,7 @@ $(SDLC_TEST_FILES_BUILT) &: $(SDLC_ALL_JS_FILES_SRC)
 		--source-maps=inline \
 		$(SRC)
 
-$(SDLC_TEST_PASS_MARKER) $(SDLC_TEST_REPORT) $(TEST_STAGING)/coverage &: package.json $(SDLC_TEST_FILES_BUILT) $(SDLC_TEST_DATA_BUILT)
+$(SDLC_TEST_PASS_MARKER) $(SDLC_TEST_REPORT) $(TEST_STAGING)/coverage &: package.json $(SDLC_TEST_FILES_BUILT) # $(SDLC_TEST_DATA_BUILT)
 	rm -rf $@
 	mkdir -p $(dir $@)
 	echo -n 'Test git rev: ' > $(SDLC_TEST_REPORT)
